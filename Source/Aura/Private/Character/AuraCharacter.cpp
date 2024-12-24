30594539e87877e1dca26e1a28e7ebd96927a3fc
K3Z4 <tim@makato.nl>
2024-12-24 19:36:03 +0100

    Ad Camera

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,20 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFrameWork/CharacterMovementComponent.h"
+
+
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	
+	GetCharacterMovement()->bConstrainToPlane = true;
+
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
